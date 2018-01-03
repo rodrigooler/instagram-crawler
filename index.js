770c95407ceff741ba4d6f2967167432c05d774e
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-01-03 21:40:58 -0200

    Add headers and rawHeaders

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ const Crawler = require('crawler');
 
 const c = new Crawler({
   maxConnections: 10,
-  // This will be called for each crawled page
-  callback: function(error, res, done) {
+  callback: (error, res, done) => {
     if (error) {
       console.log(error);
     } else {
-
-			console.log(`==============================`);
-			console.log(res);
-			console.log(`==============================`);
-      var $ = res.$;
-      // $ is Cheerio by default
-      //a lean implementation of core jQuery designed specifically for the server
-			console.log($('meta').text());
-
+			const { headers, rawHeaders, $ } = res;
 
       const result = JSON.stringify({
+        headers,
+        rawHeaders,
         title: $('title')
           .text()
           .replace('\n', ''),
